30c840ff99e2b236fdadbb5071c000a94b1e200f
Rafidh Chowdhury <saucyspacefries@gmail.com>
2025-07-11 14:49:29 -0400

    Some Serialization Stuff and Diagnostics

--- Triskai.Serialization/BinarySerializer.cs
+++ Triskai.Serialization/BinarySerializer.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization.Formatters.Binary;
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using Triskai.Core;
 
 
--- core/DiagnosticsSystem/interfaces/ILogger.cs
+++ core/DiagnosticsSystem/interfaces/ILogger.cs
@@ -2,7 +2,7 @@ namespace Triskai.Core
 {
     public interface ILogger
     {
-        void Log(string message, LogLevel level = LogLevel.Info);
+        void Log(string message, LogLevel level = LogLevel.Log);
         void LogWarning(string message);
         void LogError(string message);
         void LogDebug(string message);
